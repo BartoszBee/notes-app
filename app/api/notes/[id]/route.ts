1031a8442a5e8bc66529c74e4f6da265ae99f30e
bartoszb <b.biernacki@delta.poznan.pl>
2025-07-24 13:55:07 +0200

    route change put context

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,24 +1,28 @@
 import { NextResponse } from "next/server";
 import db from "@/lib/db";
 
-export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
+
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function DELETE(_: Request, context: Context) {
+  const { id } = context.params;
   const stmt = db.prepare("DELETE FROM notes WHERE id = ?");
-  stmt.run(params.id);
+  stmt.run(id);
   return NextResponse.json({ success: true });
 }
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: Request, context: Context) {
+  const { id } = context.params;
   const { title, content } = await req.json();
+
   const stmt = db.prepare(
     "UPDATE notes SET title = ?, content = ? WHERE id = ?"
   );
-  stmt.run(title, content, params.id);
+  stmt.run(title, content, id);
 
-  return NextResponse.json({ id: params.id, title, content });
+  return NextResponse.json({ id, title, content });
 }
